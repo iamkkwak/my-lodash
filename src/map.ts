6e03be53526ac7c041d0552ecc51095f2ae9bbea
은정 Rachel <kkwak96@gmail.com>
2021-07-22 23:24:15 +0900

    feat: make `map` compatible with object type

--- src/map.ts
+++ src/map.ts
@@ -1,4 +1,13 @@
-export function map(array: Array<unknown>, callback: (...args) => boolean): Array<unknown> {
+import {Collection, DefaultCallback} from './types';
+
+// TODO: filter에 createCallback 함수 재사용하기
+function createCallbackWithKey(key: PropertyKey): DefaultCallback {
+  return (value) => {
+    return value[key];
+  };
+}
+
+function arrayMap(array: Array<unknown>, callback: DefaultCallback): Array<unknown> {
   const length = array ? array.length : 0;
   const result = new Array(length);
   for (let i = 0; i < length; i += 1) {
@@ -6,4 +15,24 @@ export function map(array: Array<unknown>, callback: (...args) => boolean): Arra
   }
 
   return result;
+}
+
+function objectMap(object: Record<PropertyKey, unknown>, callback: DefaultCallback): Array<unknown> {
+  const keys = Object.keys(object);
+  const {length} = keys;
+  const result = new Array(length);
+  for (let index = 0; index < length; index += 1) {
+    const key = keys[index];
+    result[index] = callback(object[key], key, object);
+  }
+
+  return result;
+}
+
+export function map(collection: Collection, callback: DefaultCallback | PropertyKey): Array<unknown> {
+  if (typeof callback !== 'function') {
+    callback = createCallbackWithKey(callback);
+  }
+
+  return Array.isArray(collection) ? arrayMap(collection, callback) : objectMap(collection, callback);
 }
